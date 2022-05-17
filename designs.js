65d68f84a59854dfad7a9231b51f1194c5cdcec8
jayers92 <jayers92@gmail.com>
2022-05-17 05:59:08 -0600

    first commit

---
+++ designs.js
@@ -0,0 +1,27 @@
+const tableCanvas = document.getElementById('pixelCanvas').addEventListener("click", makeGrid)
+const color = document.getElementById('colorPicker');
+​const addRow = document.getElementById('inputHeight');
+const addColumn = document.getElementById('inputWidth');
+​
+​
+const form = document.getElementId('sizePicker');
+
+form.addEventListener('submit', Function(Event) {
+    
+});
+
+for(let x = 0; i < 21; i++){
+    let numRow = document.createElement("tr");
+    numRow.id = "row + i";
+
+    tbl.appendChild(numRow)
+    let rowW = document.getElementById("row" + i);
+
+    for(let j = 0; j < 21; j++){
+        let myCell = document.createElement("td");
+        rowW.appendChild(myCell);
+    }
+ 
+ }
+
+
