5b3076ff5331bff76e87b2079c125c2498cc5c34
jayers92 <99706818+jayers92@users.noreply.github.com>
2022-05-24 06:07:37 -0600

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,20 +1,23 @@
 const form = document.getElementById('sizePicker');
-form.addEventListener('submit', function (event) {
+form.addEventListener('submit', function(event) {
     event.preventDefault();  
     const tblElement = document.getElementById('pixelCanvas');
 
-    for (let x = 0; x < 31; x = x + 1);{
+    const numHeight = document.getElementById('inputHeight').value;
+
+    for(let x = 0; x < numHeight; x = x + 1) {
         const rowElement = document.createElement('tr');
         tblElement.appendChild(rowElement);
-    }
-        for(let j = 0; j < 31; j = j + 1);{
+    
+        const numWidth = document.getElementById('inputWidth').value;
+
+        for(let y = 0; y < numWidth; y = y + 1) {
             const columnElement = rowElement.insertCell();
             rowElement.appendChild(columnElement);
         }
+    }
             columnElement.addEventListener('click', function(event) {
                 const color = document.getElementById('colorPicker').value;
                 event.target.style.backgroundColor = color;
-            });
         });
-
-
+});
