1809b8a62615504751409715f56fb8f37a8e17ca
jayers92 <99706818+jayers92@users.noreply.github.com>
2022-05-25 15:26:31 -0600

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,25 +1,25 @@
 const form = document.getElementById("sizePicker");
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const tblElement = document.getElementById("pixelCanvas");
-  tblElement.innerHTML = "";
+form.addEventListener("submit", function(event) {
+	event.preventDefault();
+	const tblElement = document.getElementById("pixelCanvas");
+	tblElement.innerHTML = "";
 
-  const numHeight = document.getElementById("inputHeight").value;
+	const numHeight = document.getElementById("inputHeight").value;
 
-  for (let x = 0; x < numHeight; x = x + 1) {
-    const rowElement = document.createElement("tr");
-    tblElement.appendChild(rowElement);
+	for (let x = 0; x < numHeight; x = x + 1) {
+		const rowElement = document.createElement("tr");
+		tblElement.appendChild(rowElement);
 
-    const numWidth = document.getElementById("inputWidth").value;
+		const numWidth = document.getElementById("inputWidth").value;
 
-    for (let y = 0; y < numWidth; y = y + 1) {
-      const columnElement = rowElement.insertCell();
-      rowElement.appendChild(columnElement);
+		for (let y = 0; y < numWidth; y = y + 1) {
+			const columnElement = rowElement.insertCell();
+			rowElement.appendChild(columnElement);
 
-      columnElement.addEventListener("click", function (event) {
-        const color = document.getElementById("colorPicker").value;
-        event.target.style.backgroundColor = color;
-      });
-    }
-  }
+			columnElement.addEventListener("click", function(event) {
+				const color = document.getElementById("colorPicker").value;
+				event.target.style.backgroundColor = color;
+			});
+		}
+	}
 });
